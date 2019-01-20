14e901d7173c14922ca9889e4651aa9382d8d915
rquanx <runquant@gmail.com>
2019-01-20 23:04:23 +0800

    none: commit all

--- commitAll.sh
+++ commitAll.sh
@@ -1,6 +1,4 @@
 for i in */.git; 
 do 
-( echo $i; cd $i/..; ../Note/commit.sh "none" "commit all"; ); 
+(echo $i; cd $i/..; if [[ `git status --porcelain` ]]; then echo "changes"; ../Note/commit.sh "none" "commit all"; else echo "No changes"; fi;); 
 done
-
-# if [[ `git status --porcelain` ]]; then echo "Changes"; else echo "No changes"; fi;
