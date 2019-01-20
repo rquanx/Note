70c5e92d34a2464d48c5421ca98e8a65c7daff87
rquanx <runquant@gmail.com>
2019-01-20 22:49:51 +0800

    none: change pull.sh

--- pull.sh
+++ pull.sh
@@ -1,2 +1,14 @@
+if [ $1 == "a" ]
+then
+    echo "a";
+    for i in */.git; 
+    do 
+            (echo $i; 
+            cd $i/..; 
+            git pull;) 
+    done;
+else 
+    echo "single";
+    git pull;
+fi
 
-for i in */.git; do (echo $i; cd $i/..; git pull;); done;
